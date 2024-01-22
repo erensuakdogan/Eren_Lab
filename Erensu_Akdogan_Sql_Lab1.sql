aaa6b9879c406884ff2418f79d5293245da19fe4
erensuakdogan <erensuakdogan@hotmail.com>
2024-01-22 18:10:04 +0100

    day 1

---
+++ Erensu_Akdogan_Sql_Lab1.sql
@@ -0,0 +1,11 @@
+INSERT INTO cars (vin, manufacturer, model, model_year, color)
+values ('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
+('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
+('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
+('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
+('DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
+
+INSERT INTO customers (cust_id, cust_name, cust_phone, cust_email, cust_address, cust_city, cust_state, cust_country, cust_zipcode)
+values (10001, 'Pablo Picasso', 0034636176382, 'Paseo de la Chopera', '14', 'Madrid', 'Madrid', 'Spain', '28045'),
+(20001, 'Abraham Lincoln', 0013059077086, '120 SW 8th St', 'Miami', 'Florida, "USA", '33130'),
+(30001, 'Napoléon Bonaparte', 0033179754000, '40 Rue du Colisée', 'Paris', 'Île-de-France, 'France', '75008')
